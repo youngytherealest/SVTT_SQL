4a5937ff89deb703bd08be4493c40198eb7cac36
giangcse <giangpt.vlg@vnpt.vn>
2023-08-25 10:47:13 +0700

    update

--- app/config.py
+++ app/config.py
@@ -1,10 +1,14 @@
-import pyodbc
-
-server = '10.91.13.128'
-database = 'SVTT'
-username = 'sa'
-password = 'Vnpt@123'
+import psycopg2
 
 def create_connection():
-    conn = pyodbc.connect(f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password};CHARSET=UTF8')
-    return conn
+    try:
+        # Kết nối đến cơ sở dữ liệu PostgreSQL
+        conn = psycopg2.connect(host="dpg-cjjbm5b37aks73f08n6g-a.singapore-postgres.render.com", database="svtt", user="root", password="EPDmJgQuSl0frtdPXTgCSqpMzjuoumtM")
+
+        # Tạo một con trỏ
+        cursor = conn.cursor()
+
+        return cursor
+    finally:
+        # Đóng kết nối đến cơ sở dữ liệu PostgreSQL
+        conn.close()
--- data/bulk-data.py
+++ data/bulk-data.py
@@ -1,26 +1,36 @@
 import pandas as pd
 import pyodbc
+import psycopg2
 
 server = '10.91.13.128'
 database = 'SVTT'
 username = 'sa'
 password = 'Vnpt@123'
 
-conn = pyodbc.connect(f'DRIVER={{SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password};CHARSET=UTF8')
-
+m_conn = pyodbc.connect(f'DRIVER={{SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password};CHARSET=UTF8')
+p_conn = psycopg2.connect(host="dpg-cjjbm5b37aks73f08n6g-a.singapore-postgres.render.com", database="svtt", user="root", password="EPDmJgQuSl0frtdPXTgCSqpMzjuoumtM")
 # Đối với pyodbc
-cursor = conn.cursor()
+m_cursor = m_conn.cursor()
+p_cursor = p_conn.cursor()
 
-df = pd.read_json(f'xaphuong.json', encoding='utf8')
+# nganh: dict = {
+#     1: 'Công nghệ thông tin',
+#     2: 'Công nghệ thông tin (CLC)',
+#     3: 'Kỹ thuật phần mềm',
+#     4: 'Kỹ thuật phần mềm (CLC)',
+#     5: 'Hệ thống thông tin',
+#     6: 'Mạng máy tính và Truyền thông',
+#     7: 'Khoa học máy tính'
+# }
 
-insert_query = """
-INSERT INTO XaPhuong(DiaChi)
-VALUES (?)
-"""
-for i in df.itertuples(index=False):
-    # Thực hiện chèn dữ liệu
-    cursor.execute(insert_query, i.province)
-    conn.commit()
+# def find_key_by_value(dictionary, value):
+#     for key, val in dictionary.items():
+#         if val == value:
+#             return key
+#     return None
+m_result = m_cursor.execute("SELECT * FROM SinhVien").fetchall()
 
-# Đóng kết nối sau khi hoàn thành
-conn.close()
+for i in m_result:
+    p_cursor.execute(f"CALL UpdateNhomThucTapBySVID('{str(i[1])}', '{i[11]}')")
+    p_conn.commit()
+    print(f'Inserted {i[1]}')

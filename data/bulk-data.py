32286e0183c39c2e0b3b4f5d79f88691365d0fb6
giangcse <giangpt.vlg@vnpt.vn>
2023-08-25 17:02:42 +0700

    update

--- data/bulk-data.py
+++ data/bulk-data.py
@@ -11,16 +11,12 @@ conn = pyodbc.connect(f'DRIVER={{SQL Server}};SERVER={server};DATABASE={database
 # Đối với pyodbc
 cursor = conn.cursor()
 
-df = pd.read_json(f'xaphuong.json', encoding='utf8')
-
-insert_query = """
-INSERT INTO XaPhuong(DiaChi)
-VALUES (?)
-"""
-for i in df.itertuples(index=False):
-    # Thực hiện chèn dữ liệu
-    cursor.execute(insert_query, i.province)
-    conn.commit()
+find = cursor.execute("SELECT MSSV FROM SinhVien WHERE Truong='VLUTE'").fetchall()
+for i in find:
+    if len(i[0])==8:
+        cursor.execute("UPDATE SinhVien SET MaLop=? WHERE MSSV=?", f'Đại học Công nghệ thông tin 20{i[0][:2]}', i[0])
+        cursor.commit()
+        print(f'Updated {i[0]}')
 
 # Đóng kết nối sau khi hoàn thành
 conn.close()

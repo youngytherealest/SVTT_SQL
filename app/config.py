d1df06598958b59a3f95f450a58193e9dbc08ea0
giangcse <giangpt.vlg@vnpt.vn>
2023-11-20 14:38:00 +0700

    update code, reqs

--- app/config.py
+++ app/config.py
@@ -1,9 +1,16 @@
-import pyodbc
+from dotenv import load_dotenv
+from pathlib import Path
 
-server = '27.74.191.209'
-database = 'SVTT'
-username = 'sa'
-password = 'Admin@2808'
+import pyodbc
+import os
+
+dotenv_path = Path('.env')
+load_dotenv(dotenv_path=dotenv_path)
+
+server = os.getenv('SQL_HOST')
+database = os.getenv('SQL_DATABASE')
+username = os.getenv('SQL_USERNAME')
+password = os.getenv('SQL_PASSWORD')
 
 def create_connection():
     conn = pyodbc.connect(f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password};CHARSET=UTF8')
